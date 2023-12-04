b04026c2fdacb0babadb3a717908c644a043f44e
Fernando PC-Escritorio <fernando95f4@gmail.com>
2023-12-04 16:49:44 -0500

    Cambio pendiente de un archivo, por el comentario.

--- KrakeDev-PrimerosPasos/4.Fundamentos-JavaScript/Reto5-CorreccionErrores/Errores-Correcciones/funcionesRobot3.js
+++ KrakeDev-PrimerosPasos/4.Fundamentos-JavaScript/Reto5-CorreccionErrores/Errores-Correcciones/funcionesRobot3.js
@@ -1,6 +1,7 @@
 autoeliminar = function(){
     alert("El robot ha sido eliminado");
 }
+// La solucion fue sacar la funcion anidada para que funcionen por serpado.
 explotar = function(){
     alert("El robot ha explotado");
 }
