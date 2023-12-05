eeec4c43b2736616725736ef4e43de6351bd7971
Fernando PC-Escritorio <fernando95f4@gmail.com>
2023-12-05 13:06:02 -0500

    Reto 8 concluido con exito.

---
+++ KrakeDev-PrimerosPasos/5.Manipulacion-Componentes/Reto8-EncenderCompu/funcionalidad.js
@@ -0,0 +1,11 @@
+encender = function () {
+  let estadoOn;
+  estadoOn = document.getElementById("imagen");
+  estadoOn.src = "img/compuPrendida.PNG";
+};
+
+apagar = function () {
+  let estadoOff;
+  estadoOff = document.getElementById("imagen");
+  estadoOff.src = "img/compuApagada.png";
+};
---
+++ KrakeDev-PrimerosPasos/5.Manipulacion-Componentes/componentes.js
@@ -0,0 +1,11 @@
+modificarTexto = function () {
+  let cmpTitulo1;
+  cmpTitulo1 = document.getElementById("titulo1");
+  cmpTitulo1.innerText = "Texto Modificado.";
+};
+
+modificarImagen = function () {
+  let cmpImagen1;
+  cmpImagen1 = document.getElementById("imagen1");
+  cmpImagen1.src = "imagenNueva.jpg";
+};
