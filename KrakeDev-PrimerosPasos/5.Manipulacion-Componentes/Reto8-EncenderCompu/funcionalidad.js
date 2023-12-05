07d1e469e57f634b7f197c97b98256d7d03789d4
Fernando PC-Escritorio <fernando95f4@gmail.com>
2023-12-05 13:16:02 -0500

    Nuevo Test de extensiones

--- KrakeDev-PrimerosPasos/5.Manipulacion-Componentes/Reto8-EncenderCompu/funcionalidad.js
+++ KrakeDev-PrimerosPasos/5.Manipulacion-Componentes/Reto8-EncenderCompu/funcionalidad.js
@@ -1,11 +1,11 @@
 encender = function () {
   let estadoOn;
   estadoOn = document.getElementById("imagen");
-  estadoOn.src = "../Reto8-EncenderCompu/img/compuPrendida.PNG";
+  estadoOn.src = "../Reto8-EncenderCompu/img/compuPrendida.png";
 };
 
 apagar = function () {
   let estadoOff;
   estadoOff = document.getElementById("imagen");
-  estadoOff.src = "../Reto8-EncenderCompu/img/compuApagada.PNG";
+  estadoOff.src = "../Reto8-EncenderCompu/img/compuApagada.png";
 };
